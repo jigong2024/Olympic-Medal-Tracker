38656d535c6d4f3b1900f85a50e52c56963fe405
jigong2024 <choijming21@naver.com>
2024-08-12 15:54:52 +0900

    feat: 레이아웃 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import MedalForm from "./components/MedalForm";
+import MedalList from "./components/MedalList";
+import MedalListItem from "./components/MedalList";
+import SortControls from "./components/SortControls";
 
 const App = () => {
+  const allStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  };
   return (
-    <div>
-      <header>2024 파리 올림픽</header>
+    <div style={allStyle}>
+      <h1>2024 파리 올림픽</h1>
+      <main
+        style={{
+          marginTop: "20px",
+        }}
+      >
+        <MedalForm />
+      </main>
+      <section
+        style={{
+          marginTop: "60px",
+        }}
+      >
+        <MedalList />
+      </section>
     </div>
   );
 };
--- src/components/MedalForm.jsx
+++ src/components/MedalForm.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 const MedalForm = () => {
-  return <div>MedalForm</div>;
+  const formContainerStyle = {
+    display: "flex",
+    flexDirection: "row",
+    gap: "60px",
+  };
+
+  const fieldGroupStyle = {
+    display: "flex",
+    flexDirection: "column",
+    width: "200px",
+    textAlign: "center",
+  };
+
+  return (
+    <form style={formContainerStyle}>
+      <label style={fieldGroupStyle}>
+        국가명
+        <input type="text" name="countryName" />
+      </label>
+      <label style={fieldGroupStyle}>
+        금메달
+        <input type="number" name="goldMedals" min="0" />
+      </label>
+      <label style={fieldGroupStyle}>
+        은메달
+        <input type="number" name="goldMedals" min="0" />
+      </label>
+      <label style={fieldGroupStyle}>
+        동메달
+        <input type="number" name="goldMedals" min="0" />
+      </label>
+      <div>
+        <button
+          style={{
+            marginRight: "20px",
+          }}
+          type="submit"
+        >
+          국가 추가
+        </button>
+        <button type="button">업데이트</button>
+      </div>
+    </form>
+  );
 };
 
 export default MedalForm;
